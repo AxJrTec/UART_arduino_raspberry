0e9bf95b9e119ee3d03de81a43133eace355a719
AxJrTec <A01738607@tec.mx>
2025-05-20 11:29:47 -0600

    Update Recepcion.py

--- Recepcion.py
+++ Recepcion.py
@@ -3,7 +3,7 @@ import serial
 puerto_Serial = serial.Serial('/dev/ttyACM0', 9600)
 print(puerto_Serial.name)
 
-while True:
+for i in range(7):
     mensaje = puerto_Serial.readline().decode('utf-8').rstrip()
     print(mensaje)
     
